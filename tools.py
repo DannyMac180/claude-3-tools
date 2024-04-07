cc566bbf97e6a66006f88e84ddc2e7718a77724f
Daniel McAteer <danielmcateer@Daniels-MacBook-Pro.local>
2024-04-07 15:38:35 -0400

    progress on examples

--- tools.py
+++ tools.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_weather(location, unit):
+def get_weather(location):
     response = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={os.getenv('OPENWEATHER_API_KEY')}"
     )
